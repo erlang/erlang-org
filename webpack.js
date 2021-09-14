0444c263b3544e9665f99bd7c026e255ed99d08b
Lukas Larsson <lukas@erlang.org>
2021-09-14 15:20:59 +0200

    Generate correct js source-maps

--- webpack.js
+++ webpack.js
@@ -5,7 +5,6 @@ module.exports = {
         "doc-search": './assets/doc-search.js'
     },
     devtool: 'source-map',
-    devtool: 'inline-source-map',
     output: {
         filename: '[name].bundle.js',
         path: resolve(__dirname, './assets/js/')
