73cb2fa12397ef4ab05f7540b27c9655fe2ba7c9
Lukas Larsson <lukas@erlang.org>
2021-09-14 09:24:30 +0200

    Use default number of hits per page for algolia

--- assets/doc-search.js
+++ assets/doc-search.js
@@ -6,18 +6,12 @@ docsearch({
     container: '#docsearch',
     indexName: 'erlang_org',
     appId: 'T71IQF5OFV',
-    apiKey: 'a2c3c6c0120e6eb2574bb328bd316967',
-    searchParameters: {
-        hitsPerPage: 10
-    }
+    apiKey: 'a2c3c6c0120e6eb2574bb328bd316967'
 });
 
 docsearch({
     container: '#docsearch-mobile',
     indexName: 'erlang_org',
     appId: 'T71IQF5OFV',
-    apiKey: 'a2c3c6c0120e6eb2574bb328bd316967',
-    searchParameters: {
-        hitsPerPage: 10
-    }
+    apiKey: 'a2c3c6c0120e6eb2574bb328bd316967'
 });
