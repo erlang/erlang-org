d5f1c4dc46781cf8cf325cc57b937f039d27fb1c
Lukas Larsson <lukas@erlang.org>
2021-09-14 11:49:40 +0200

    Always refetch latest docs

--- _scripts/download-docs.sh
+++ _scripts/download-docs.sh
@@ -26,11 +26,12 @@ RINCLUDE=()
 for VSN in ${MAJOR_VSNs}; do
     LATEST_VSN=$(_get_latest_vsn "^OTP-${VSN}")
     ARCHIVE="docs/otp_doc_html_${LATEST_VSN}.tar.gz"
-    if [ ! -f "${ARCHIVE}" ] && [ ! -f "docs/${VSN}/${LATEST_VSN}" ]; then
+    if [ "${VSN}" = "24" ]; then
+        echo "Checking for ${LATEST_VSN} on garazdawi github"
+        curl --silent --location --fail --show-error "${HDR[@]}" "https://github.com/garazdawi/otp/releases/download/OTP-24.0.6-doc/otp_doc_html_24.0.6.tar.gz" > "${ARCHIVE}";
+    elif [ ! -f "${ARCHIVE}" ] && [ ! -f "docs/${VSN}/${LATEST_VSN}" ]; then
         echo "Checking for ${LATEST_VSN} on github"
-        if [ "${VSN}" = "24" ]; then
-            curl --silent --location --fail --show-error "${HDR[@]}" "https://github.com/garazdawi/otp/releases/download/OTP-24.0.6-doc/otp_doc_html_24.0.6.tar.gz" > "${ARCHIVE}";
-        elif ! curl --silent --location --fail --show-error "${HDR[@]}" "https://github.com/erlang/otp/releases/download/OTP-${LATEST_VSN}/otp_doc_html_${LATEST_VSN}.tar.gz" > "${ARCHIVE}"; then
+        if ! curl --silent --location --fail --show-error "${HDR[@]}" "https://github.com/erlang/otp/releases/download/OTP-${LATEST_VSN}/otp_doc_html_${LATEST_VSN}.tar.gz" > "${ARCHIVE}"; then
             rm -f "${ARCHIVE}"
             LATEST_VSN=$(_get_latest_vsn "^OTP-${VSN}\.[0-9] ")
             if [ ! -f "docs/${VSN}/${LATEST_VSN}" ]; then
