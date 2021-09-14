66257b05aec60595e86c25b53fdbd763829edfa0
Lukas Larsson <lukas@erlang.org>
2021-09-14 10:35:15 +0200

    Fix issues found by google lighthouse

--- assets/doc-search.js
+++ assets/doc-search.js
@@ -9,9 +9,11 @@ docsearch({
     apiKey: 'a2c3c6c0120e6eb2574bb328bd316967'
 });
 
-docsearch({
-    container: '#docsearch-mobile',
-    indexName: 'erlang_org',
-    appId: 'T71IQF5OFV',
-    apiKey: 'a2c3c6c0120e6eb2574bb328bd316967'
-});
+if (document.getElementById('docsearch-mobile')) {
+    docsearch({
+        container: '#docsearch-mobile',
+        indexName: 'erlang_org',
+        appId: 'T71IQF5OFV',
+        apiKey: 'a2c3c6c0120e6eb2574bb328bd316967'
+    });
+}
