19a9417b671fd3025b48629f418ae7bca6c83e5f
Lukas Larsson <lukas@erlang.org>
2021-09-29 17:05:03 +0200

    Fix shellcheck and svg styles

--- _scripts/download-latest-doc.sh
+++ _scripts/download-latest-doc.sh
@@ -5,7 +5,7 @@ REPO="garazdawi"
 BRANCH="lukas/erl_docgen/make-docs-more-responsive"
 REPO="https://api.github.com/repos/${REPO}/otp"
 RUN_ID=$(curl "${REPO}/actions/runs?branch=${BRANCH}&event=push&per_page=1&status=success" | jq ".workflow_runs[0].id")
-ARTIFACT=$(curl ${REPO}/actions/runs/${RUN_ID}/artifacts | jq -r '.artifacts[] | select(.name == "otp_doc_html") | .archive_download_url')
+ARTIFACT=$(curl "${REPO}/actions/runs/${RUN_ID}/artifacts" | jq -r '.artifacts[] | select(.name == "otp_doc_html") | .archive_download_url')
 curl --location -H "Authorization: token ${GITHUB_TOKEN}" "${ARTIFACT}" > /tmp/doc.zip
 unzip /tmp/doc.zip && rm -f /tmp/doc.zip
-mv otp_doc_html.tar.gz docs/otp_doc_html_${LATEST}.tar.gz
+mv otp_doc_html.tar.gz "docs/otp_doc_html_${LATEST}.tar.gz"
