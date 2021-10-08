7a2f3cf16700ba4e71c674f7e65c18643a5b5824
Lukas Larsson <lukas@erlang.org>
2021-10-08 10:41:02 +0200

    Use gh module to fetch releases

--- _scripts/src/create-releases.erl
+++ _scripts/src/create-releases.erl
@@ -1,6 +1,5 @@
 -module('create-releases').
 -include_lib("kernel/include/logger.hrl").
--include_lib("public_key/include/OTP-PUB-KEY.hrl").
 -export([main/1]).
 
 main([OTPVersionTable, ReleasesJson, PatchesDir]) ->
@@ -10,7 +9,7 @@ main([OTPVersionTable, ReleasesJson, PatchesDir]) ->
     Versions = parse_otp_versions_table(OTPVersionTable),
     Downloads = parse_erlang_org_downloads(),
     Tags = parse_github_tags(),
-    {ok, GHReleases} = ghget("/repos/erlang/otp/releases"),
+    {ok, GHReleases} = gh:get("/repos/erlang/otp/releases"),
     Releases = maps:map(fun(Key,Val) -> process_patches(Key,Val,Downloads,Tags,GHReleases) end, Versions),
     ok = file:write_file(ReleasesJson, create_release_json(Releases)),
     create_patches(PatchesDir, Releases),
@@ -61,8 +60,9 @@ parse_erlang_org_downloads() ->
       end, #{}, Downloads).
 
 download_erlang_org_downloads() ->
-    {ok,{{_,200,_},Hdrs,Body}} = httpc:request(get,{"https://erlang.org/download",[]},
-                                               ssl_opts("https://erlang.org/download"),[]),
+    {ok,{{_,200,_},Hdrs,Body}} = httpc:request(
+        get,{"https://erlang.org/download",[]},
+        ssl_opts("https://erlang.org/download"),[]),
     case lists:member({"content-encoding","gzip"}, Hdrs) of
         true ->
             zlib:gunzip(Body);
@@ -71,7 +71,7 @@ download_erlang_org_downloads() ->
     end.
 
 parse_github_tags() ->
-    {ok, Json} = ghget("/repos/erlang/otp/tags"),
+    {ok, Json} = gh:get("/repos/erlang/otp/tags"),
     maps:from_list(
       [begin
            TagName = maps:get(<<"name">>,Tag),
@@ -179,47 +179,6 @@ create_patch(Dir, Patch, ReadmeStr) ->
             "---\n",
             otp_readme:render(Readme)]).
 
-ghget(Url) ->
-    ghget(Url,[]).
-ghget(Url, GetHdrs) ->
-    Auth = case os:getenv("GITHUB_TOKEN") of
-               false -> [];
-               Token ->
-                   [{"Authorization","token " ++ Token}]
-           end,
-    FullUrl =
-        case string:prefix(Url, "https://") of
-            nomatch ->
-                "https://api.github.com" ++ Url ++ "?per_page=100";
-            _match ->
-                Url
-        end,
-    Accept = proplists:get_value("Accept", GetHdrs, "application/vnd.github.v3+json"),
-    case httpc:request(
-           get,
-           {FullUrl,
-            [{"Accept", Accept},
-             {"User-Agent","erlang-httpc"} | Auth]},ssl_opts(FullUrl),[{body_format,binary}]) of
-        {ok,{{_,200,_},Hdrs,Body}} when Accept =:= "application/vnd.github.v3+json" ->
-            case lists:keyfind("link",1,Hdrs) of
-                false ->
-                    {ok, jsone:decode(Body)};
-                {"link",Link} ->
-                    %% If there is a link header, the results are paginated, so
-                    %% we follow the pages until the end.
-                    case re:run(Link,"<([^>]+)>; rel=\"next\"",[{capture,all_but_first,binary}]) of
-                        nomatch ->
-                            {ok, jsone:decode(Body)};
-                        {match,[NextLink]} ->
-                            {ok, NextJson} = ghget(NextLink),
-                            {ok, jsone:decode(Body) ++ NextJson}
-                    end
-            end;
-        {ok,{{_,200,_},_Hdrs,Body}} ->
-            {ok, Body};
-        Else ->
-            {error, Else}
-    end.
 
 
 pmap(Fun, List) ->
@@ -233,54 +192,14 @@ pmap(Fun, List) ->
              Value
      end || Ref <- Refs].
 
-
-
-ssl_opts(Url) ->
-    #{ host := Hostname } = uri_string:parse(Url),
-    VerifyFun = {fun ssl_verify_hostname:verify_fun/3,
-                  [{check_hostname, Hostname}]},
-    %% CACerts = certifi:cacerts(),
-    [{ssl,[{verify, verify_peer},
-           {cacertfile, "/etc/ssl/certs/ca-certificates.crt"},
-           {verify_fun, VerifyFun},
-           {customize_hostname_check,
-            [{match_fun, public_key:pkix_verify_hostname_match_fun(https)}]}
-          ]}].
-
-%% This code is gratefully copied from rebar3_util.erl
-%% It is currently not used as we try to use the OSs bundled certificates instead
-%% but I leave it down here in case it is needed in the future.
--ifdef(USE_CERTIFI).
-ssl_opts_certify(Url) ->
+    ssl_opts(Url) ->
         #{ host := Hostname } = uri_string:parse(Url),
-    VerifyFun = {fun ssl_verify_hostname:verify_fun/3,
-                 [{check_hostname, Hostname}]},
-    CACerts = certifi:cacerts(),
-    [{ssl,[{verify, verify_peer},
-           {cacerts, CACerts},
-           {partial_chain, fun partial_chain/1},
-           {verify_fun, VerifyFun},
-           {customize_hostname_check,
-            [{match_fun, public_key:pkix_verify_hostname_match_fun(https)}]}]
-     }].
-partial_chain(Certs) ->
-    Certs1 = [{Cert, public_key:pkix_decode_cert(Cert, otp)} || Cert <- Certs],
-    CACerts = certifi:cacerts(),
-    CACerts1 = [public_key:pkix_decode_cert(Cert, otp) || Cert <- CACerts],
-    case lists:search(fun({_, Cert}) ->
-                               check_cert(CACerts1, Cert)
-                       end, Certs1) of
-        {value, Trusted} ->
-            {trusted_ca, element(1, Trusted)};
-        false ->
-            unknown_ca
-    end.
-
-extract_public_key_info(Cert) ->
-    ((Cert#'OTPCertificate'.tbsCertificate)#'OTPTBSCertificate'.subjectPublicKeyInfo).
-
-check_cert(CACerts, Cert) ->
-    lists:any(fun(CACert) ->
-                      extract_public_key_info(CACert) == extract_public_key_info(Cert)
-              end, CACerts).
--endif.
+        VerifyFun = {fun ssl_verify_hostname:verify_fun/3,
+                      [{check_hostname, Hostname}]},
+        %% CACerts = certifi:cacerts(),
+        [{ssl,[{verify, verify_peer},
+               {cacertfile, "/etc/ssl/certs/ca-certificates.crt"},
+               {verify_fun, VerifyFun},
+               {customize_hostname_check,
+                [{match_fun, public_key:pkix_verify_hostname_match_fun(https)}]}
+              ]}].
