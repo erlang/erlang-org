7210fde1c458049f31fa0e7bf8146be2d4ac0b67
Lukas Backström <lukas@erlang.org>
2025-05-21 15:54:06 +0200

    Use LATEST_MAJOR_VSN as part of doc hash
    We do this to trigger a complete cache update when a new major release
    is released.

--- _scripts/download-docs.sh
+++ _scripts/download-docs.sh
@@ -14,6 +14,7 @@ HDR=(--silent --location --fail --show-error -H "Authorization: ${TOKEN}" -H "X-
 
 # The files that are involved when generating docs
 SCRIPT_FILES="_scripts/download-docs.sh _scripts/otp_flatten_docs _scripts/otp_flatten_ex_docs _scripts/otp_doc_sitemap.sh"
+LATEST_MAJOR_VSN=$(echo "$MAJOR_VSNs" | tr ' ' '\n' | sort -n | tail -1)
 
 _get_vsns() {
     grep "${1}" "${OTP_VERSIONS_TABLE}" | awk '{print $1}' | sed 's/OTP-\(.*\)/\1/g'
@@ -25,7 +26,7 @@ _get_latest_vsn() {
 
 _get_doc_hash() {
     # shellcheck disable=SC2086
-    (echo "${1}" && cat ${SCRIPT_FILES}) | sha256sum | awk '{print $1}'
+    (echo "${1}" && echo "${LATEST_MAJOR_VERSION}" && cat ${SCRIPT_FILES}) | sha256sum | awk '{print $1}'
 }
 
 _flatten_docs() {
@@ -37,7 +38,6 @@ _flatten_docs() {
 }
 
 MAJOR_VSNs=$(_get_vsns "OTP-[0-9]\+\.0 " | sed 's/^\([0-9]\+\).*/\1/g')
-LATEST_MAJOR_VSN=$(echo "$MAJOR_VSNs" | tr ' ' '\n' | sort -n | tail -1)
 RINCLUDE=()
 
 for VSN in ${MAJOR_VSNs}; do
